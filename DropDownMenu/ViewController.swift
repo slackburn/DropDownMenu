dcfb0bd1bee1888ad3f6ca21ba337a32cc41d9de
slackburn <s.blackburn4@icloud.com>
2018-11-06 15:56:24 +0000

    Created project and created first dropdown menu.

--- DropDownMenu/ViewController.swift
+++ DropDownMenu/ViewController.swift
@@ -14,7 +14,31 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    @IBOutlet weak var selectButton: UIButton!
+    @IBOutlet var items: [UIButton]!
+    
+    
+    @IBAction func selectButtonSelected(_ sender: UIButton) {
+        selectButton.titleLabel?.textAlignment = NSTextAlignment.center
+        selectButton.setTitleColor(UIColor.black, for: .normal)
+        items.forEach { (Button) in
+            UIView.animate(withDuration: 0.3, animations: { Button.isHidden = !Button.isHidden
+                self.view.layoutIfNeeded()
+            })
+        }
+    }
+    
+    @IBAction func itemsSelected(_ sender: UIButton) {
+        selectButton.titleLabel?.text = sender.titleLabel?.text //changes menu label
+        items.forEach { (button) in
+            UIView.animate(withDuration: 0.3, animations: {
+                button.isHidden = !button.isHidden
+                self.view.layoutIfNeeded()
+            })
+        }
+    }
+    
 
 }
 
